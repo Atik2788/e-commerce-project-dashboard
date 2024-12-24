e0bfc0d6669e983aa3d3c919cd9bb8665fc0677b
Atik2788 <ashanto653.2@gmail.com>
2024-12-24 23:01:47 +0600

    completed design in register page

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login Page</h1>
+  return (
+    <div className="min-w-screen min-h-screen bg-[#8ae1db] flex justify-center items-center">
+      <div className="w-[350px] text-[#ffffff] p-2">
+        <div className="bg-[#36c1c8] p-4 rounded-md">
+          <h2 className="text-xl mb-3 font-bold">Welcome to Ecommerce</h2>
+          <p className="text-sm mb-3 font-medium">
+            Please register your account
+          </p>
+
+          <form>
+            <div className="flex flex-col w-full gap-1 mb-3 ">
+                 <label htmlFor="name">Name</label>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>Register Page</h1>
+  return (
+    <div className="min-w-screen min-h-screen bg-[#8ae1db] flex justify-center items-center">
+      <div className="w-[350px] text-[#ffffff] p-2">
+        <div className="bg-[#36b9c0] p-4 rounded-md">
+          <h2 className="text-xl mb-3 font-bold">Welcome to Ecommerce</h2>
+          <p className="text-sm mb-3 font-medium">
+            Please register your account
+          </p>
+
+          <form>
+            <div className="flex flex-col w-full gap-1 mb-3 ">
+              <label htmlFor="name">Name</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
+                type="text"
+                name="name"
+                placeholder="Name"
+                id="name"
+                required
+              />
+            </div>
+
+            <div className="flex flex-col w-full gap-1 mb-3 ">
+              <label htmlFor="email">Email</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
+                type="email"
+                name="email"
+                placeholder="Email"
+                id="email"
+                required
+              />
+            </div>
+
+            <div className="flex flex-col w-full gap-1 mb-3 ">
+              <label htmlFor="password">Password</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md"
+                type="password"
+                name="passworde"
+                placeholder="Password"
+                id="password"
+                required
+              />
+            </div>
+
+            <div className="flex items-center w-full gap-3 mb-3 ">
+              <input
+                className="w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray300 focus:ring-blue-500 "
+                type="checkbox"
+                name="checkbox"
+                id="checkbox"
+              />
+              <label htmlFor="checkbox">
+                I agree to privacy policy & trems
+              </label>
+            </div>
+
+            <button className="bg-slate-600 w-full hover:shadow-blue-300 /50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">Sign Up</button>
+          </form>
         </div>
-    );
+      </div> 
+    </div>
+  );
 };
 
 export default Register;
