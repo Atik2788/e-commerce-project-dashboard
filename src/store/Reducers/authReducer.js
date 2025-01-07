f155ea8cbbc880067a324e48bfa070a1176dbe92
Atik2788 <ashanto653.2@gmail.com>
2025-01-07 09:00:25 +0600

    get the error message when the admin email dont match

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -4,12 +4,13 @@ import api from "../../api/api";
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
     async(info) =>{
-        console.log(info);
+        // console.log(info);
         try {
             const {data} = await api.post('/admin-login', info, {withCredentials: true})  
-            console.log(data); 
+            console.log(data);  
+
         } catch (error) {
-            
+            console.log(error.response.data.error);
         }
     }
 )
