4199990dfe646dcef8f04e29e5e536ffa0f2c82f
Atik2788 <ashanto653.2@gmail.com>
2025-01-11 23:14:47 +0600

    setup admin routes js

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,1 +1,12 @@
+// import AdminDashboard from "../../views/admin/AdminDashboard";
+import {lazy} from "react";
+const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'))
 
+
+export const adminRoutes = [
+    {
+        path: 'admin/dashboard',
+        element: <AdminDashboard/>,
+        role: "admin"
+    }
+]
