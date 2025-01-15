cade52b83c95313f993a08af83df0ac0acba7b33
Atik2788 <ashanto653.2@gmail.com>
2025-01-15 21:11:46 +0600

    done to show hide sidebar

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import { getNav } from "../navigation";
 import { BiLogOutCircle } from "react-icons/bi";
 
-const Sidebar = () => {
+const Sidebar = ({showSidebar, setShowSidebar }) => {
   const { pathname } = useLocation();
   const [allNav, setAllNave] = useState([]);
 
@@ -16,10 +16,12 @@ const Sidebar = () => {
 
   return (
     <div>
-      <div></div>
+      <div onClick={() => setShowSidebar(false)} className={`fixed duration-200 w-screen h-screen bg-[#385e8080] top-0 left-0 z-10 ${!showSidebar ? 'invisible' : 'visible'}`}>
+
+      </div>
 
       <div
-        className={`w-[260px] fixed bg-[#39a290] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all `}
+        className={`w-[260px] fixed bg-[#39a290] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${showSidebar ? 'left-0 ': '-left-[260px] lg:left-0'}`}
       >
         <div className="h-[70px] flex justify-center items-center">
           <Link to="/" className="w-[180px] h-[30px] ">
