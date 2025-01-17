2e791d06a35e0a85d16a925aad70666d6d531b8e
Atik2788 <ashanto653.2@gmail.com>
2025-01-17 21:17:27 +0600

    done admin dashboard chart

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import AdminDashboardHeader from './AdminDashboardHeader';
-
-
+import AdminDashboardChart from './AdminDashboardChart';
 
 const AdminDashboard = () => {
+
+
     return (
         <div className='px-2 md:px-7 py-5'>
           <AdminDashboardHeader/>
+          <AdminDashboardChart/>
 
-          <div className='w-full flex flex-wrap mt-7'>
-            <div className="w-full lg:w-7/12 lg:pr-3 ">
-              <div className='w-full bg-[#39a290] p-4 rounded-md '>
-
-              </div>
-            </div>
-          </div>
 
 
         </div>
---
+++ src/views/admin/AdminDashboardChart.jsx
@@ -0,0 +1,81 @@
+import Chart from 'react-apexcharts';
+
+const AdminDashboardChart = () => {
+    
+    const state = {
+        series: [
+          {
+            name: 'Orders',
+            data: [23, 34, 45, 56, 34, 16, 75, 90, 55, 60, 55, 80 ]
+          },
+          {
+            name: 'Revenue',
+            data: [63, 36, 14, 78, 54, 16, 26, 85, 16, 46, 50, 75 ]
+          },
+          {
+            name: 'Sellers',
+            data: [40, 36, 46, 25, 87, 69, 58, 85, 47, 75, 45, 85 ]
+          }
+        ],
+        options: {
+          color: ['#181ee8', '#181ee8'],
+          plotOptions: {
+            radius: 30
+          },
+          charts: {
+            background: 'transparent',
+            foreColor:  '#d0d2d6'
+          },
+          dataLabels: {
+            enabled: false
+          },
+          strock: {
+            show: true,
+            curve: ['smooth', 'straight', 'stepline'],
+            line: 'butt',
+            colors: '#f0f0f0',
+            width: .5,
+            dashArray: 0
+          },
+          xaxis: {
+            categories: ["Jan", "Feb", "Mar", "Apl", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+          },
+          legend: {
+            position: 'top'
+          },
+          responsive: [
+            {
+                breakpoint: 565,
+                yaxis: {
+                    categories: ["Jan", "Feb", "Mar", "Apl", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+                  },
+                options: {
+                    plotOptions: {
+                        bar: {
+                            horizontal: true
+                        }
+                      },
+                    chrt: {
+                        height: '550px'
+                    }
+                },
+
+            }
+          ]
+        }
+    }
+
+    return (
+        <div>
+           <div className='w-full flex flex-wrap mt-7'>
+            <div className="w-full lg:w-7/12 lg:pr-3 ">
+              <div className='w-full bg-[#39a290] p-4 rounded-md '>
+                <Chart options={state.options} series={state.series} type='bar' height={350}/>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+};
+
+export default AdminDashboardChart;
