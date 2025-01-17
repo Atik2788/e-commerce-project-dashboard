e29ac4992a544ea7c41c22aadf62d99a556393ba
Atik2788 <ashanto653.2@gmail.com>
2025-01-17 17:15:56 +0600

    starting admin  deshboard design

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const AdminDashboard = () => {
     return (
-        <div>
-          <h1>Admin Dashboard</h1>
+        <div className='px-2 md:px-7 py-5'>
+          <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7">
+            <div className="flex justify-between items-center p-5 "></div>
+          </div>
         </div>
     );
 };
