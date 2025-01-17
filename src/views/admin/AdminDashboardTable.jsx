a4298282a3417d590abd32fd19f88e640d0a851c
Atik2788 <ashanto653.2@gmail.com>
2025-01-17 23:22:22 +0600

    starting design on admin dashboard table

--- src/views/admin/AdminDashboardTable.jsx
+++ src/views/admin/AdminDashboardTable.jsx
@@ -10,10 +10,26 @@ const AdminDashboardTable = () => {
       </div>
 
       <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-[#d0d2d6] uppercase border-b border-slate-700"> 
-          <tr>
-            <th className="py-3 px-4">Order Id</th>
-          </tr>
+        <table className="w-full text-sm text-[#d0d2d6] text-center">
+          <thead className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
+            <tr>
+              <th className="py-3 px-4">Order Id</th>
+              <th className="py-3 px-4">Price</th>
+              <th className="py-3 px-4">Payment Status</th>
+              <th className="py-3 px-4">Order Status</th>
+              <th className="py-3 px-4">Active </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap ">#34665</td>
+                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap ">$499</td>
+                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap ">Pending</td>
+                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap ">Pending</td>
+                <td scope="row" className="py-3 px-4 font-medium whitespace-nowrap ">View</td>
+            </tr> 
+          </tbody>
         </table>
       </div>
     </div>
