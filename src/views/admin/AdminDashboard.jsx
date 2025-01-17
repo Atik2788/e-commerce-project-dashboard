74a99a2c892653170af243aecb88b248dd906ac1
Atik2788 <ashanto653.2@gmail.com>
2025-01-17 18:32:34 +0600

    completed admin dashboard header

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { MdCurrencyExchange, MdOutlineProductionQuantityLimits, MdOutlineShoppingCart   } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+
 
 const AdminDashboard = () => {
     return (
         <div className='px-2 md:px-7 py-5'>
           <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7">
-            <div className="flex justify-between items-center p-5 "></div>
+
+            <div className="flex justify-between items-center p-5 bg-[#dbf6c4] rounded-md gap-3">
+              <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
+                <h2 className='text-3xl font-bold '>$3569</h2>
+                <span className='text-md font-medium'>Total Sales</span>
+              </div>
+              
+              <div className='w-[40px] h-[47px] rounded-full bg-red-600 flex justify-center items-center text-xl'>
+              <MdCurrencyExchange className='text-[#fae8e8] shadow-lg'/>
+              </div>
+            </div>
+
+
+            <div className="flex justify-between items-center p-5 bg-[#ccdcfe] rounded-md gap-3">
+              <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
+                <h2 className='text-3xl font-bold '>50</h2>
+                <span className='text-md font-medium'>Products</span>
+              </div>
+              
+              <div className='w-[40px] h-[47px] rounded-full bg-purple-600 flex justify-center items-center text-xl'>
+              <MdOutlineProductionQuantityLimits  className='text-[#fae8e8] shadow-lg'/>
+              </div>
+            </div>
+
+
+            <div className="flex justify-between items-center p-5 bg-[#caf6c4] rounded-md gap-3">
+              <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
+                <h2 className='text-3xl font-bold '>10</h2>
+                <span className='text-md font-medium'>Sellers</span>
+              </div>
+              
+              <div className='w-[40px] h-[47px] rounded-full bg-green-600 flex justify-center items-center text-xl'>
+              <FaUsers  className='text-[#fae8e8] shadow-lg'/>
+              </div>
+            </div>
+            
+
+            <div className="flex justify-between items-center p-5 bg-[#fbd4ec] rounded-md gap-3">
+              <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
+                <h2 className='text-3xl font-bold '>75</h2>
+                <span className='text-md font-medium'>Orders</span>
+              </div>
+              
+              <div className='w-[40px] h-[47px] rounded-full bg-blue-700 flex justify-center items-center text-xl'>
+              <MdOutlineShoppingCart  className='text-[#fae8e8] shadow-lg'/>
+              </div>
+            </div>
+
           </div>
         </div>
     );
