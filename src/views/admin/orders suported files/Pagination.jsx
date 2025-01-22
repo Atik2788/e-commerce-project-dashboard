da86a1265a8ce92108f6de25603acc91dceebba0
Atik2788 <ashanto653.2@gmail.com>
2025-01-22 07:21:35 +0600

    working on pagination for order page 2(done shoing the btn)

--- src/views/admin/orders suported files/Pagination.jsx
+++ src/views/admin/orders suported files/Pagination.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { RxDoubleArrowLeft } from "react-icons/rx";
 
 const Pagination = ({currentPage, setCurrentPage, totalItem, parPage, showItem}) => {
     
@@ -13,14 +13,33 @@ const Pagination = ({currentPage, setCurrentPage, totalItem, parPage, showItem})
     let endPage = startPage < 0 ? showItem : showItem + startPage;
 
     if(startPage <= 0){
-        
+        startPage = 1
+    }
+
+    const createBtn = () =>{
+        const btns = [];
+        for(let i = startPage; i < endPage; i++ ){
+            btns.push(
+                <li onClick={() => setCurrentPage(i)} className={`${currentPage === i ? "bg-[#8ae1db] shadow-lg shadow-[#6db0ac] text-[#b7b9be]" : "bg-slate-600 hover:bg-[#8ae1db] shadow-lg hover:shadow-[#6db0ac] text-[#d0d2d6] hover:text-[#b7b9be]"} w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer`}>
+                    {i}
+                </li>
+            )
+        }
+        return btns;
     }
 
     return (
-        <div>
-            
-        </div>
+        <ul className='flex gap-3'>
+            {
+                currentPage > 1 && <li className="w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-600 text-[#d0d2d6]">
+                    <RxDoubleArrowLeft />
+                </li>
+            }{
+                createBtn()
+            }
+        </ul>
     );
+
 };
 
 export default Pagination;
