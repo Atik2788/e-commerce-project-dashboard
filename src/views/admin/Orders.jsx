2d828f6ed6dfe0a15d98ea604a0b1aca7d090af6
Atik2788 <ashanto653.2@gmail.com>
2025-01-22 07:45:53 +0600

    order page deshign done

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -10,7 +10,6 @@ const Orders = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <div>
       <div className="px-2 lg:px-7 pt-5">
         <div className="w-full p-4 bg-[#39a290] rounded-md ">
           <OrdersHeader setParPage={setParPage} />
@@ -18,16 +17,19 @@ const Orders = () => {
           {/* table */}
           <OrdersTable show={show} setShow={setShow} />
 
-          <Pagination
-            setCurrentPage={setCurrentPage}
-            currentPage={currentPage}
-            totalItem = {50}
-            parPage={parPage}
-            showItem={3}
-          />
+          <div className="w-full flex justify-end mt-4 bottom-4 right-4">
+            <Pagination
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+              totalItem={50}
+              parPage={parPage}
+              showItem={3}
+            />
+          </div>
+
+
         </div>
       </div>
-    </div>
   );
 };
 
--- src/views/admin/orders suported files/Pagination.jsx
+++ src/views/admin/orders suported files/Pagination.jsx
@@ -1,7 +1,6 @@
-import { RxDoubleArrowLeft } from "react-icons/rx";
+import { RxDoubleArrowLeft, RxDoubleArrowRight } from "react-icons/rx";
 
 const Pagination = ({currentPage, setCurrentPage, totalItem, parPage, showItem}) => {
-    
     let totalPage = Math.ceil(totalItem / parPage);
     // current is pageNumber
     let startPage = currentPage;
@@ -18,7 +17,7 @@ const Pagination = ({currentPage, setCurrentPage, totalItem, parPage, showItem})
 
     const createBtn = () =>{
         const btns = [];
-        for(let i = startPage; i < endPage; i++ ){
+        for(let i = startPage; i <= endPage; i++ ){
             btns.push(
                 <li onClick={() => setCurrentPage(i)} className={`${currentPage === i ? "bg-[#8ae1db] shadow-lg shadow-[#6db0ac] text-[#b7b9be]" : "bg-slate-600 hover:bg-[#8ae1db] shadow-lg hover:shadow-[#6db0ac] text-[#d0d2d6] hover:text-[#b7b9be]"} w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer`}>
                     {i}
@@ -31,11 +30,19 @@ const Pagination = ({currentPage, setCurrentPage, totalItem, parPage, showItem})
     return (
         <ul className='flex gap-3'>
             {
-                currentPage > 1 && <li className="w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-600 text-[#d0d2d6]">
+                currentPage > 1 && 
+                <li onClick={() =>setCurrentPage(currentPage - 1) } className="w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-600 text-[#d0d2d6] cursor-pointer">
                     <RxDoubleArrowLeft />
                 </li>
-            }{
+            }
+            {
                 createBtn()
+            }
+                        {
+                currentPage < totalPage && 
+                <li onClick={() =>setCurrentPage(currentPage + 1) } className="w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-600 text-[#d0d2d6] cursor-pointer">
+                    <RxDoubleArrowRight />
+                </li>
             }
         </ul>
     );
