5d3fd0c4417d5435bff816cf73678522bbf6a75c
Atik2788 <ashanto653.2@gmail.com>
2025-01-23 20:47:42 +0600

    21 working on category right side section

--- src/views/admin/Category.jsx
+++ src/views/admin/Category.jsx
@@ -3,6 +3,7 @@ import SetParPage from "./commonPages/SetParPage";
 import Pagination from "./commonPages/Pagination";
 import AdminDashboardTable from "./admin suported files/AdminDashboardTable";
 import Table from "./commonPages/Table";
+import { FaImages } from "react-icons/fa6";
 
 const Category = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -42,8 +43,7 @@ const Category = () => {
 
         {/* left side div */}
         <div className="w-full lg:w-7/12">
-
-          <div className="w-full p-4 bg-[#39a290] rounded-md ">
+          <div className="w-full p-4 bg-[#39a290] rounded-md">
             <SetParPage setParPage={setParPage} />
 
             <Table headers={headers} rows={rows}/>
@@ -58,17 +58,30 @@ const Category = () => {
               />
             </div>
           </div>
-
-
-        {/* right side div */}
-          <div className="w-full lg:w-5/12">
-
-          </div>
         </div>
 
 
-        <div>
+        {/* right side div */}
+        <div className={`w-[320px] lg:w-5/12 translate-x-100 lg:relative lg:right-0 fixed ${show ? "right-0" : '-right-[340px]'} z-20 top-0 transition-all duration-500`}>
+          <div className="w-full pl-5">
+            <div className="bg-[#39a290] rounded-md h-screen lg:h-auto px-3 py-2 text-[#d0d2d6]">
+              <h1 className="text-[#d0d2d6] font-semibold text-xl mb-4 w-full text-center">Add Category</h1>
+              <form>
+                <div className="flex flex-col w-full gap-1 mb-3">
+                  <label htmlFor="">Category Name</label>
+                  <input className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#f9fbfb] rounded-md text-[#39a290]" type="text" id="name" name="category_name" placeholder="Category Name" />
+                </div>
 
+                <div>
+                  <label className="flex justify-center items-center flex-col h-[238px] cursor-pointer border border-dashed hover:border-[#8ae1db] w-full border-white" htmlFor="image">
+                    <span><FaImages /></span>
+                    <span>Select Image</span>
+                  </label>
+                </div>
+
+              </form>
+            </div>
+          </div>
         </div>
 
 
