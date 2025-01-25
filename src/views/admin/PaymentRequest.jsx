874b982fbb6c3ee11aa455affaca5a4d1b5be328
Atik2788 <ashanto653.2@gmail.com>
2025-01-25 22:26:53 +0600

    23

--- src/views/admin/PaymentRequest.jsx
+++ src/views/admin/PaymentRequest.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { FixedSizeList as List } from 'react-window';
+import React from "react";
+import { FixedSizeList as List } from "react-window";
 
 const PaymentRequest = () => {
-    return (
-        <div className='px-2 lg:px-7 pt-5'>
-            <div className="w-full p-4 bg-[#39a290] rounded-md">
-                <h2 className='text-xl font-medium pb-5 text-[#d0d2d6]'>Withdrawl Request</h2>
+  return (
+    <div className="px-2 lg:px-7 pt-5">
+
+      <div className="w-full p-4 bg-[#39a290] rounded-md">
+        <h2 className="text-xl font-medium pb-5 text-[#d0d2d6]">
+          Withdrawl Request
+        </h2>
+
+        <div className="w-full">
+          <div className="w-full overflow-x-auto">
+            <div className="flex bg-[#8ae1db] rounded-sm uppercase text-xs font-bold min-w-[340px]">
+              <div className="w-[25%] p-2">No</div>
+              <div className="w-[25%] p-2">Amount</div>
+              <div className="w-[25%] p-2">Status</div>
+              <div className="w-[25%] p-2">Date</div>
+              <div className="w-[25%] p-2">Action</div>
+              <div className="w-[25%] p-2">Number</div>
             </div>
+          </div>
         </div>
-    );
+        
+      </div>
+
+
+    </div>
+  );
 };
 
 export default PaymentRequest;
