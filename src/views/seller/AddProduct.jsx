dcc4df5d6209e16a6804525d3df056861139179d
Atik2788 <ashanto653.2@gmail.com>
2025-01-31 00:17:19 +0600

    28 done on  seller add product pages design

--- src/views/seller/AddProduct.jsx
+++ src/views/seller/AddProduct.jsx
@@ -3,31 +3,31 @@ import { Link } from 'react-router-dom';
 
 const AddProduct = () => {
 
-    const categoriys = [
+    const categorys = [
         {
             id: 1,
-            nme: 'Sports'
+            name: 'Sports'
         },
         {
             id: 2,
-            nme: 'Tshirt'
+            name: 'Tshirt'
         },
         {
             id: 3,
-            nme: 'Mobile'
+            name: 'Mobile'
         },
         {
             id: 4,
-            nme: 'Watch'
+            name: 'Watch'
         },
         {
             id: 5,
-            nme: 'Computer'
+            name: 'Computer'
         },
         {
             id: 6,
-            nme: 'Pant'
-        },
+            name: 'Pant'
+        }
     ]
 
     const [state, setState] = useState({
@@ -49,11 +49,20 @@ const AddProduct = () => {
 
     const [cateShow, setCateShow] = useState(false);
     const [category, setCategory] = useState("");
-    const [allCategory, setAllCategory]= useState([]);
-    const [searchValue, setsSearchValue]= useState('');
+    const [allCategory, setAllCategory]= useState(categorys);
+    const [searchValue, setSearchValue]= useState('');
+    console.log(searchValue);
 
     const categorySearch = (e) =>{
         const value = e.target.value
+        setSearchValue(value)
+        if (value) {
+            let srcValue = allCategory.filter(c => c.name.toLowerCase().indexOf(value.toLowerCase()) > -1)
+            setAllCategory(srcValue)
+        }
+        else{
+            setAllCategory(categorys)
+        }
     }
 
 
@@ -75,7 +84,7 @@ const AddProduct = () => {
 
                         {/* first product row start */}
                         <div className="flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]">
-                            {/* first product name in a row */}
+                            {/* product name section */}
                             <div className="flex flex-col w-full gap-1 ">
                                 <label htmlFor="name">Product Name</label>
                                 <input 
@@ -84,7 +93,7 @@ const AddProduct = () => {
                                 className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#8ae1db] rounded-md text-[#39a290]'
                                 />
                             </div>
-                            {/* second product name in a row */}
+                            {/* brand section */}
                             <div className="flex flex-col w-full gap-1 ">
                                 <label htmlFor="brand">Product Brand</label>
                                 <input 
@@ -97,40 +106,47 @@ const AddProduct = () => {
                         </div>
                          {/* first product row end */}
 
+
                         {/* secont product row start */}
                         <div className="flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]">
-                            {/* first product name in a row */}
+
+                            {/* Category section start*/}
                             <div className="flex flex-col w-full gap-1 relative">
                                 <label htmlFor="category">Category</label>
                                 <input
                                 readOnly onClick={() => setCateShow(!cateShow)} 
                                 onChange={inputHandle} 
-                                value={state.name} type="text" name='category' id='category' placeholder='Category'
+                                value={category} type="text" name='category' id='category' placeholder='-- select category--'
                                 className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#8ae1db] rounded-md text-[#39a290]'
                                 />
-                                <div className={`absolute top-[101%] bg-slate-800 w-full transition-all ${cateShow ? "scale-100" : "scale-0"}`}>
+                                <div className={`absolute top-[101%] bg-[#277367] w-full transition-all ${cateShow ? "scale-100" : "scale-0"}`}>
                                     <div className="w-full px-4 py-2 fixed">
                                         <input 
+                                        value={searchValue}
                                         onChange={categorySearch}
-                                        className='px-3 py-1 w-full focus:border-indigo-500 outline-none bg-transparent border border-slate-700 rounded-md text-[#d0d2d6] overflow-hidden' type="text" placeholder='Search' />
+                                        className='px-3 py-1 w-full focus:border-[#8ae1db] outline-none bg-transparent border border-slate-700 rounded-md text-[#d0d2d6] overflow-hidden' type="text" placeholder='Search' />
                                     </div>
                                     <div className="pt-14"></div>
-                                    <div className="flex justify-start items-start flex-col h-[200px] overflow-x-scroll">
+                                    <div className="flex justify-start items-start flex-col h-[200px] ">
                                         {
-                                            allCategory.map((c, i) => <span onClick={() => {
+                                            allCategory.map((c, i) => <span 
+                                            key={i}
+                                            className={`px-4 py-2 w-full text-[#d0d2d6] cursor-pointer hover:bg-[#8ae1db] hover:text-black ${category === c.name && 'bg-[#39a290]'}`}
+                                            onClick={() => {
                                                 setCateShow(false)
                                                 setCategory(c.name)
-                                                setsSearchValue("")
-                                                setAllCategory(category)
+                                                setSearchValue("")
+                                                setAllCategory(categorys)
                                             }}>{c.name}</span>)
                                         }
                                     </div>
 
                                 </div>
 
-                            </div>
+                            </div>                            
+                            {/* Category section end*/}
 
-                            {/* second product name in a row */}
+                            {/* stock section start*/}
                             <div className="flex flex-col w-full gap-1 ">
                                 <label htmlFor="stock">Product Stock</label>
                                 <input 
@@ -138,10 +154,103 @@ const AddProduct = () => {
                                 value={state.stock} type="text" name='stock' id='stock' placeholder='Stock'
                                 className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#8ae1db] rounded-md text-[#39a290]'
                                 />
-                            </div>                            
+                            </div> 
+                            {/* stock section end*/}                           
+                        </div>
                          {/* second product row end */}
 
+
+                        {/* 3rd product row start */}
+                        <div className="flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]">
+
+                            {/* price name section */}
+                            <div className="flex flex-col w-full gap-1 ">
+                                <label htmlFor="price">Price</label>
+                                <input 
+                                onChange={inputHandle} 
+                                value={state.price} 
+                                type="number" 
+                                name='price' 
+                                id='price' 
+                                placeholder='Price'
+                                className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#8ae1db] rounded-md text-[#39a290]'
+                                />
+                            </div>
+
+                            {/* discount section */}
+                            <div className="flex flex-col w-full gap-1 ">
+                                <label htmlFor="discount">Discount</label>
+                                <input 
+                                onChange={inputHandle} 
+                                value={state.brand} 
+                                type="number" 
+                                name='discount' 
+                                id='discount' 
+                                placeholder='Discount by %'
+                                className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#8ae1db] rounded-md text-[#39a290]'
+                                />
+                            </div>                         
+
                         </div>
+                         {/* 3rd product row end */}
+
+                         {/* 3rd product row start */}
+                        <div className="flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]">
+
+                            {/* price name section */}
+                            <div className="flex flex-col w-full gap-1 ">
+                                <label htmlFor="price">Price</label>
+                                <input 
+                                onChange={inputHandle} 
+                                value={state.price} 
+                                type="number" 
+                                name='price' 
+                                id='price' 
+                                placeholder='Price'
+                                className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#8ae1db] rounded-md text-[#39a290]'
+                                />
+                            </div>
+
+                            {/* discount section */}
+                            <div className="flex flex-col w-full gap-1 ">
+                                <label htmlFor="discount">Discount</label>
+                                <input 
+                                onChange={inputHandle} 
+                                value={state.discount} 
+                                type="number" 
+                                name='discount' 
+                                id='discount' 
+                                placeholder='Discount by %'
+                                className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#8ae1db] rounded-md text-[#39a290]'
+                                />
+                            </div>
+
+                            
+
+                        </div>
+                         {/* 3rd product row end */}
+
+
+                         {/* description product row start */}
+                        <div className="flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]">
+                            {/* discount section */}
+                            <div className="flex flex-col w-full gap-1 ">
+                                <label htmlFor="description">Description</label>
+                                <textarea 
+                                onChange={inputHandle} 
+                                value={state.description} 
+                                type="text" 
+                                name='description' 
+                                id='description' 
+                                cols={10}
+                                rows={4}
+                                placeholder='Description'
+                                className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#8ae1db] rounded-md text-[#39a290]'
+                                ></textarea>
+                            </div>
+                        </div>
+                        {/* description product row end */}
+
                     </form>
                 </div>
                 {/* product add form end */}
